5f9c667e216f86538f4a8ebce17e2d9538541876
jbosslady <jblepany@outlook.fr>
2026-03-03 01:53:18 +0100

    created class to parse conf file. tried to be more synthetic and added redirection

---
+++ include/config.hpp
@@ -0,0 +1,51 @@
+#ifndef CONFIG_HPP
+# define CONFIG_HPP
+
+# include <map>
+# include <set>
+# include <string>
+
+struct rewriteConfig {
+	std::string pattern;
+	std::string replacement;
+
+	bool operator<(const rewriteConfig& other) const;
+};
+
+struct cgiConfig {
+	std::string extension;
+	std::string executable_path;
+
+	std::set<std::string> allowed_methods; // by default, all methods are allowed
+	bool operator<(const cgiConfig& other) const;
+};
+
+struct locationConfig {
+	std::string path;
+	std::set<std::string> allowed_methods;
+	std::string root;
+
+	bool 	autoindex;
+	bool 	upload_auth;
+	std::string upload_path;
+
+	std::set<std::string> index_files;
+
+	std::set<rewriteConfig> rewrites;
+	std::set<cgiConfig> cgi_configs;
+
+	bool operator<(const locationConfig& other) const;
+};
+
+struct serverConfig {
+	int port;
+
+	std::map<int, std::string> error_pages;
+	size_t	client_max_body_size;
+
+	std::set<locationConfig> locations;
+
+	bool operator<(const serverConfig& other) const;
+};
+
+# endif
