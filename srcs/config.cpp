51e415b9e46758b4fc15347417834836b38166c2
jbosslady <jblepany@outlook.fr>
2026-03-04 01:40:04 +0100

    use of stat to detect non regular file of unexcepted size

--- srcs/config.cpp
+++ srcs/config.cpp
@@ -31,27 +31,35 @@ config::config(const std::string & filename) {
 
 // methods to parse config file
 int config::parseConfFile(std::set<serverConfig> & servers) const {
-	if (checkFile(config_file.c_str()))
+	if (checkFile(_config_file.c_str()) == -1)
 		return -1;
+	(void)servers;
+	(void)_buffer;
 	return 0;
 }
 
 int	config::checkFile(const char *__restrict__ file_path) const {
-	if (access(file_path, F_OK | R_OK)) {
-		log_error<std::string>("config file cannot be open");
+	if (access(file_path, F_OK | R_OK) == -1) {
 		log_error<std::string>(strerror(errno));
 		return -1;
 	}
 	struct stat st;
 	if (stat(file_path,  &st) == -1) {
-		log_error<std::string>("config file cannot be stat");
 		log_error<std::string>(strerror(errno));
 		return -1;
 	}
-	if (access(file_path, R_OK) == -1) {
-		log_error<std::string>("config file cannot be read");
-		log_error<std::string>(strerror(errno));
+	if (st.st_size == 0) {
+		log_error<std::string>("config file is empty");
 		return -1;
 	}
+	if (st.st_size > 1024 * 1024) {
+		log_error<std::string>("config file seems too large");
+		return -1;
+	}
+	if (!S_ISREG(st.st_mode)) {
+		log_error<std::string>("config file must be a regular disk file");
+		return -1;
+	}
+	// File is a regular, readable, non-empty file of reasonable size
 	return 0;
 }
